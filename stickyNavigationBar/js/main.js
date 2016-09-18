ee2a226b2fc931f729d42910e3efb4f88daca30a
micahpotts <mrs.micahpotts@gmail.com>
2016-09-18 17:03:40 -0400

    Added sticky-start and sticky-end events.

--- stickyNavigationBar/js/main.js
+++ stickyNavigationBar/js/main.js
@@ -7,3 +7,11 @@ $(".animsition").animsition({
 });
 
 $(".header").sticky();
+
+$(".header").on("sticky-start", function() {
+	$(".description").html("We build <strong>great</strong> apps");
+});
+
+$(".header").on("sticky-end", function() {
+	$(".description").html("We build apps");
+});
